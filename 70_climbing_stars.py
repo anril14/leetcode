44706715da768d3fdffaa796a53326cc0f643501
Alexey Scherbakov <dlyabsinetolko@gmail.com>
2025-04-01 13:03:47 +0300

    Added alt. solution for 70 for better understanding

--- 70_climbing_stars.py
+++ 70_climbing_stars.py
@@ -1,12 +1,12 @@
-
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
-        for i in range(n - 1):
-            temp = one
-            one = one + two
-            two = temp
+        two, one = 1, 1
+        for _ in range(n - 1):
+            one_temp = one
+            one = two + one
+            two = one_temp
         return one
 
+
 solution = Solution()
-print(solution.climbStairs(5))
+print(solution.climbStairs(1))
---
+++ 70_climbing_stars_rec.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 1:
+            return 1
+
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+
+solution = Solution()
+print(solution.climbStairs(38))
