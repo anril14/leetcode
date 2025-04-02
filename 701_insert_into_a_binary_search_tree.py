9bf681c8ed7e0c14f165367e39f97a74334d5b28
Alexey Scherbakov <dlyabsinetolko@gmail.com>
2025-04-02 21:03:32 +0300

    Added solution for 701

---
+++ 701_insert_into_a_binary_search_tree.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        elif val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
+
+
+node6 = TreeNode(6)
+node4 = TreeNode(4, None, node6)
+solution = Solution()
+solution.insertIntoBST(node4, 5)
